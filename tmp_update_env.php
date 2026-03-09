6c4b85ff63d97ca80a3050d2a671a9895ba57a37
Marco Vanzo <marco@mv-consulting.it>
2026-03-09 18:15:57 +0100

    deploy: 2026-03-09 18:15

--- tmp_update_env.php
+++ tmp_update_env.php
@@ -4,13 +4,4 @@ if (file_exists($env_file)) {
     $content = file_get_contents($env_file);
     $new_content = preg_replace('/^META_CONFIG_ID=(.*)$/m', '# META_CONFIG_ID=$1', $content);
     if ($content !== $new_content) {
-        file_put_contents($env_file, $new_content);
-        echo "Successfully commented out META_CONFIG_ID in remote .env";
-    }
-    else {
-        echo "META_CONFIG_ID not found or already commented out";
-    }
-}
-else {
-    echo ".env file not found at $env_file";
-}
+ 
