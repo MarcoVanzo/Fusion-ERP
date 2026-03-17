9db68dac5377c5a8bdb4ff8264f6deaaa4748f58
Marco Vanzo <marco@mv-consulting.it>
2026-03-17 20:42:54 +0100

    deploy: 2026-03-17 20:42

--- read_errors.php
+++ read_errors.php
@@ -2,9 +2,16 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$logFile = '/web/htdocs/www.fusionteamvolley.it/home/ERP/php_errors.log';
-// Since I don't know the exact php error log path, I'll force it to a local file for this request
-ini_set('error_log', __DIR__ . '/my_debug.log');
+$logFile = __DIR__ . '/my_debug.log';
+if (file_exists($logFile)) {
+    unlink($logFile);
+    echo "Cleared old log.\n";
+}
+ini_set('error_log', $logFile);
+
+if (function_exists('opcache_reset')) {
+    opcache_reset();
+}
 
 require_once __DIR__ . '/api/Modules/Scouting/ScoutingController.php';
 
@@ -16,8 +23,8 @@ $val = $method->invoke(null, 'SCOUTING_FUSION_FORM_ID');
 var_dump($val);
 
 echo "\n--- log contents ---\n";
-if (file_exists(__DIR__ . '/my_debug.log')) {
-    echo file_get_contents(__DIR__ . '/my_debug.log');
+if (file_exists($logFile)) {
+    echo file_get_contents($logFile);
 } else {
     echo "No log generated.";
 }
