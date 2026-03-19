29ad3fc8baefff82e181096c3fad6c8ea33dc95f
Marco Vanzo <marco@mv-consulting.it>
2026-03-19 22:56:10 +0100

    deploy: 2026-03-19 22:56

--- test_ig2.php
+++ test_ig2.php
@@ -10,7 +10,7 @@ require 'api/Shared/Database.php';
 require 'api/Modules/Social/SocialRepository.php';
 require 'vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
 $dotenv->safeLoad();
 
 $repo = new \FusionERP\Modules\Social\SocialRepository();
