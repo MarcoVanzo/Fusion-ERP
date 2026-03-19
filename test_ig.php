c0cd011e010664e7f8b00222508c3ddcb802c8d0
Marco Vanzo <marco@mv-consulting.it>
2026-03-19 22:01:41 +0100

    deploy: 2026-03-19 22:01

--- test_ig.php
+++ test_ig.php
@@ -1,7 +1,9 @@
 <?php
-require 'Shared/Database.php';
-require '../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+require 'api/Shared/Database.php';
+require 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 $db = \FusionERP\Shared\Database::getInstance();
 $r = $db->query("SELECT * FROM meta_tokens ORDER BY updated_at DESC LIMIT 5")->fetchAll(PDO::FETCH_ASSOC);
