96570caa747d81b2926e842a13d7405d5a8414c2
Marco Vanzo <marco@mv-consulting.it>
2026-03-22 15:54:45 +0100

    deploy: 2026-03-22 15:54

--- create_ec_products.php
+++ create_ec_products.php
@@ -1,15 +1,15 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+require_once __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 // Allow errors to be visible for this temporary script
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/Shared/Database.php';
+require_once __DIR__ . '/api/Shared/Database.php';
 
 try {
     $db = \FusionERP\Shared\Database::getInstance();
