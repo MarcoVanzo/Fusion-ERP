270921363f987d7376ed7aec77e63ad8cf95af42
Marco Vanzo <marco@mv-consulting.it>
2026-03-27 16:41:52 +0100

    deploy: 2026-03-27 16:41

--- clear_cache.php
+++ clear_cache.php
@@ -1,7 +1,10 @@
 <?php
+header('Content-Type: text/plain');
 if (function_exists('opcache_reset')) {
-    opcache_reset();
-    echo "OPcache in RAM svuotata con successo.\n";
+    echo "OPcache Reset: " . (opcache_reset() ? 'Success' : 'Failed') . "\n";
 } else {
-    echo "OPcache non abilitato.\n";
+    echo "OPcache is not enabled or function does not exist.\n";
+}
+if (function_exists('apcu_clear_cache')) {
+    echo "APCu Reset: " . (apcu_clear_cache() ? 'Success' : 'Failed') . "\n";
 }
