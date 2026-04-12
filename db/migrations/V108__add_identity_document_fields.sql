be888c1dd582e16759beb61c2020f48722bea329
Marco Vanzo <marco@mv-consulting.it>
2026-04-12 21:44:07 +0200

    deploy: 2026-04-12 21:44

--- db/migrations/V108__add_identity_document_fields.sql
+++ db/migrations/V108__add_identity_document_fields.sql
@@ -1,4 +1,3 @@
 -- V108__add_identity_document_fields.sql
 -- No-op migration.
--- Columns identity_document were already added manually or via a separate process.
-SELECT 1;
+-- Columns identity_document already present.
