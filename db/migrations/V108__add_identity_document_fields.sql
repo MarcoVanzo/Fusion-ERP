405c2c1261dd51f6a7d80649fe456c522752a388
Marco Vanzo <marco@mv-consulting.it>
2026-04-12 21:37:57 +0200

    deploy: 2026-04-12 21:37

--- db/migrations/V108__add_identity_document_fields.sql
+++
@@ -1,5 +0,0 @@
--- V108__add_identity_document_fields.sql
--- Adds identity document field to athletes and staff units for Rooming List generation.
-
-ALTER TABLE athletes ADD COLUMN identity_document VARCHAR(255) NULL AFTER image_release_consent;
-ALTER TABLE staff_members ADD COLUMN identity_document VARCHAR(255) NULL AFTER notes;
