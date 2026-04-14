9fba209d5d987494ce050db3993634ff9c649f41
Marco Vanzo <marco@mv-consulting.it>
2026-04-14 17:37:11 +0200

    deploy: 2026-04-14 17:37

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'fusion-erp-mobile-v9';
+const CACHE_NAME = 'fusion-erp-mobile-v10';
 const ASSETS_TO_CACHE = [
   './index.html',
-  './css/style.css?v=9',
-  './js/app.js?v=9',
+  './css/style.css?v=10',
+  './js/app.js?v=10',
   './manifest.json'
 ];
 
