b81ba9759232208be3b4d9e7e388f1a50ca6f7a5
Marco Vanzo <marco@mv-consulting.it>
2026-04-20 18:28:50 +0200

    deploy: 2026-04-20 18:28

--- api/check_migrations.php
+++ api/check_migrations.php
@@ -4,7 +4,7 @@ $dotenv = Dotenv\Dotenv::createMutable(dirname(__DIR__));
 $dotenv->safeLoad();
 try {
     $db = FusionERP\Shared\Database::getInstance();
-    $stmt = $db->query("SELECT * FROM migrations");
+    $stmt = $db->query("SELECT * FROM migrations ORDER BY id DESC LIMIT 20");
     echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 } catch (Exception $e) {
     echo $e->getMessage();
