9d27d7f6a3630e5c7866e0e72ca8d2f3c5134d2f
Marco Vanzo <marco@mv-consulting.it>
2026-04-20 18:39:18 +0200

    deploy: 2026-04-20 18:39

--- api/api_ww3.php
+++ api/api_ww3.php
@@ -4,7 +4,7 @@ $dotenv = Dotenv\Dotenv::createMutable(dirname(__DIR__));
 $dotenv->safeLoad();
 try {
     $db = FusionERP\Shared\Database::getInstance();
-    $stmt = $db->query("SELECT * FROM whatsapp_messages");
+    $stmt = $db->query("SELECT id, username, email, tenant_id FROM users");
     echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 } catch (Exception $e) {
     echo $e->getMessage();
