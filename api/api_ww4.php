38c9121ccf28f806159aa610fc8b2ff1eb7549a4
Marco Vanzo <marco@mv-consulting.it>
2026-04-20 18:39:51 +0200

    deploy: 2026-04-20 18:39

--- api/api_ww4.php
+++ api/api_ww4.php
@@ -4,7 +4,7 @@ $dotenv = Dotenv\Dotenv::createMutable(dirname(__DIR__));
 $dotenv->safeLoad();
 try {
     $db = FusionERP\Shared\Database::getInstance();
-    $stmt = $db->query("SELECT id, username, email, tenant_id FROM users");
+    $stmt = $db->query("SELECT email, tenant_id FROM users");
     echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 } catch (Exception $e) {
     echo $e->getMessage();
